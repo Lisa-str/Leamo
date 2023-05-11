c03528de19b9b272d5b86eb450377d673f51a2db
Lisa-str <Lisa.Staruch@studentfr.ch>
2023-05-11 09:48:18 +0200

    carousel + fix button hover/active

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,29 @@
+// ---------- Swiper Carousel
+
+import Swiper, { Navigation, Pagination, Autoplay } from "swiper";
+
+Swiper.use([Navigation, Pagination, Autoplay]);
+
+const swiper = new Swiper(".swiper-container", {
+  clickable: true,
+  // cssMode: true,
+  loop: true,
+  speed: 1000,
+  mousewheel: true,
+  keyboard: true,
+  spaceBetween: 50,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: true,
+  },
+});
+
+// ---------- Burger Menu
+
 var burgerMenu = document.querySelector(".nav-burger-menu");
 var Nav = document.querySelector(".nav-desk");
 
